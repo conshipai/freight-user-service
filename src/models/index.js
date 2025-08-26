1b7cfec9f95dee47d5a9615ce98f79f152cc90b6
conshipai <thomas.ehler@conship.ai>
2025-08-25 21:09:11 -0500

    Create index.js

---
+++ src/models/index.js
@@ -0,0 +1,8 @@
+// src/models/index.js or individual files
+module.exports = {
+  User: require('./User'),
+  Company: require('./Company'),
+  Airport: require('./Airport'),
+  ZipCodeAirport: require('./ZipCodeAirport'),
+  // ... other models
+};
